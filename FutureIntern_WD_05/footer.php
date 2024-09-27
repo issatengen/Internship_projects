92403f76219e7e26d04fe981a2fa8d9b22645752
Issa Tengen <issatengen@outlook.com>
2024-09-27 11:48:18 +0100

    The footer of the application

---
+++ FutureIntern_WD_05/footer.php
@@ -0,0 +1,8 @@
+<!--Footer-->
+<center>
+    <br><br><br><br><br>
+    <div class="footer">
+        © Copyright <b>2024</b> All right reserved <br>
+        Design by <b><a href="mailto:issatengen12@gmail.com" style="text-decoration:none; color:white">TengTech</a></b>
+    </div>
+</center>
